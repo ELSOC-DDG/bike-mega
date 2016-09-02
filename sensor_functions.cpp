e682e8927b1d7bc5c36f5323a24a35e6ed074277
James Slack-Smith <jpslacksmith@hotmail.com.au>
2016-09-02 12:37:58 +1000

    Add sensor_initialise()

--- sensor_functions.cpp
+++ sensor_functions.cpp
@@ -8,4 +8,18 @@
 #include "Arduino.h"
 #include "bike.h"
 
+// sensor_initialise()
+//      initialises sensor pins as input pins
 
+void sensor_initialise() {
+    #ifdef DEBUG_HIGH_LEVEL
+    Serial.println('Entering sensor_initialise()'); 
+    #endif //DEBUG_HIGH_LEVEL
+
+    pinMode(voltage_pin, INPUT);
+    pinMode(current_pin, INPUT);
+
+    #ifdef DEBUG_LOW_LEVEL
+    Serial.println('Exiting sensor_initialise()');
+    #endif
+}
