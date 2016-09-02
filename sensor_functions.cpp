bf9b9352bb0daa6b314ddf9c557ef46ed7d36665
James Slack-Smith <jpslacksmith@hotmail.com.au>
2016-09-02 12:26:33 +1000

    Create sensor_functions.cpp and sensor_functions.h

---
+++ sensor_functions.cpp
@@ -0,0 +1,11 @@
+/*
+ * sensor_functions.cpp
+ * 
+ * This file contains function definitions for voltage and current sensing
+ *
+ */
+
+#include "Arduino.h"
+#include "bike.h"
+
+
---
+++ sensor_functions.h
@@ -0,0 +1,25 @@
+/*
+ * sensor_functions.h
+ *
+ * This file contains #define's and function prototypes for voltage and current sensing
+ *
+ */
+
+#ifndef SENSOR_FUNCTIONS.H
+#define SENSOR_FUNCTIONS.H
+
+/******************************
+ *          Constants         *
+ ******************************/
+
+/******************************
+ *     Function Prototypes    *
+ ******************************/
+
+
+
+
+
+
+
+#endif
